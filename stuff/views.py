01961af47a0dac6e51bfe70b114e6ac51c4bc82b
Matthew Larkin <matthewlarkin8@gmail.com>
2022-08-30 23:05:17 -0400

    Added permission class to the StuffDetail view.

--- stuff/views.py
+++ stuff/views.py
@@ -1,5 +1,6 @@
 from .models import Stuff
 from .serializers import StuffSerializer
+from .permissions import IsOwnerOrReadOnly
 from rest_framework.generics import RetrieveUpdateDestroyAPIView, ListCreateAPIView
 
 class StuffList(ListCreateAPIView):
@@ -7,5 +8,6 @@ class StuffList(ListCreateAPIView):
     serializer_class = StuffSerializer
 
 class StuffDetail(RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsOwnerOrReadOnly,)
     queryset = Stuff.objects.all()
     serializer_class = StuffSerializer
